736729a9cc4f7a2d30150e3c75a4e0f57cc2759c
Jameson Hodge <jamesondh@gmail.com>
2013-04-17 08:26:59 -0400

    Fixed comparison bug

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -14,7 +14,7 @@ def getPlugins():
 
     # import all .py files except for __init__.py and globalvars.py
     for i in contents:
-        if i.endswith('.py') is True and i is not '__init__.py' and i is not 'globalvars.py':
+        if i.endswith('.py') == True and i != '__init__.py' and i != 'globalvars.py':
             imported = importlib.import_module('plugins.' + i[:-3])
             imported.main()
             del imported
