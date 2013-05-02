3c8d168e0983227dc7d056f4abe73860d02900c6
Jameson Hodge <jamesondh@gmail.com>
2013-05-02 15:58:54 -0400

    Added Facebook plugin

---
+++ src/plugins/facebook.py
@@ -0,0 +1,18 @@
+import sys
+import webbrowser
+import plugins
+
+
+def main():
+
+    # open facebook
+    if plugins.phrase.lower().startswith("open facebook") or plugins.phrase.lower().startswith("open face book"):
+        webbrowser.open("http://facebook.com/")
+        print "Opening Facebook"
+        sys.exit(0)
+
+    # open my facebook
+    if plugins.phrase.lower().startswith("open my facebook") or plugins.phrase.lower().startswith("open my face book"):
+        webbrowser.open("http://facebook.com/")
+        print "Opening Facebook"
+        sys.exit(0)
